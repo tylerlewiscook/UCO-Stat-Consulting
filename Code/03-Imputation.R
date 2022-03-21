a0278bb12cc001dfd15fd1a927e5226f6137f55b
tylerlewiscook <8811690+tylerlewiscook@users.noreply.github.com>
2022-03-21 17:49:43 -0500

    Add files via upload

---
+++ Code/03-Imputation.R
@@ -0,0 +1,32 @@
+# Imputation
+
+# MICE -------------------------------------------------------------------------
+# https://www.jstatsoft.org/article/view/v045i03
+# https://rdrr.io/cran/mice/man/mice.html
+
+library(mice)
+nhanes
+
+summary(nhanes)
+md.pattern(nhanes)
+
+imp1 <- mice(nhanes, method = "mean", m = 1, maxit = 1)
+complete(imp1)
+
+imp2 <- mice(nhanes)
+imp2
+complete(imp2)
+
+
+# missForest -------------------------------------------------------------------
+# https://academic.oup.com/bioinformatics/article/28/1/112/219101
+
+library(missForest)
+imp3 <- missForest(nhanes)
+imp3
+
+nhanes2 <- nhanes
+nhanes2$hyp <- as.factor(nhanes2$hyp)
+imp4 <- missForest(nhanes2)
+imp4
+imp4$ximp
